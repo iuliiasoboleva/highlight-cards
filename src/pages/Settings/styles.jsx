9ebb3456504cf34a67b6e2ced1e7d3666b635b93
del0ff <itspec-ib@yandex.ru>
2025-10-06 13:41:53 +0300

    Сделал правки по шрифту

--- src/pages/Settings/styles.jsx
+++ src/pages/Settings/styles.jsx
@@ -369,7 +369,9 @@ export const SmallList = styled.ul`
   list-style: none;
   color: gray;
   font-size: 10px;
-  font-weight: 500;
+  font-weight: 200;
+  font-family: 'Manrope', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', Arial, sans-serif;
   display: grid;
   gap: 6px;
 
