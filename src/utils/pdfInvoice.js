445cf1fa752e9518d8f6b6262af1cf4e9d73ee31
del0ff <itspec-ib@yandex.ru>
2025-11-25 16:24:43 +0300

    делаю смену реквизитов

--- src/utils/pdfInvoice.js
+++ src/utils/pdfInvoice.js
@@ -229,9 +229,9 @@ export function buildReceiverDefaults() {
   return {
     name: 'ООО "ЛОЯЛ КЛАБ"',
     inn: '9715518580',
-    kpp: '770301001',
+    kpp: '771501001',
     bank_name: 'ООО "Банк Точка"',
-    checking_account: '40702810520000230757',
+    checking_account: '40702810520000256427',
     correspondent_account: '30101810745374525104',
     bik: '044525104',
     signatory: 'Генеральный директор Яковлева Регина Ринатовна',
